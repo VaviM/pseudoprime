279b4ba104f3c6bce31ba0c1cc82977ec286c11e
Valentina Maslennikova <46162222+VaviM@users.noreply.github.com>
2019-12-02 23:33:51 +0200

    ver. 0.1
    Search pseudoprime numbers on interval

---
+++ RGR/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // PseudoPrime.Sequence First = new PseudoPrime.Sequence(new List<uint> { 2,3,5,7,11},5);//the same as next
+            for (uint i = 0; i < 13; i++)
+            {
+                PseudoPrime.Sequence First = new PseudoPrime.Sequence(5, i);
+                PseudoPrime.Sequence Second = new PseudoPrime.Sequence(new List<uint> { 2, 3, 5, 7, 13 }, i);
+                PseudoPrime.CreateOperators(ref First);
+                PseudoPrime.CreateOperators(ref Second);
+                PseudoPrime.CreateSequences(ref First, ref Second);
+                PseudoPrime.CompareSequences(First, Second);
+                Console.WriteLine(new string('*', 25));
+            }
+        }
+    }
+}
